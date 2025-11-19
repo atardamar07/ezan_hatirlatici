27c49c8880649e95522bec7bb34a9f2b54d35c31
atardamar07 <atardamar07@gmail.com>
2025-11-19 05:59:53 +0300

    auto update

--- lib/services/ad_ids.dart
+++ lib/services/ad_ids.dart
@@ -1,12 +1,12 @@
 class AdIds {
   // Uygulamayı store'a yüklerken yalnızca bu ID'leri güncellemeniz yeterli.
-  static const androidAppId = 'ca-app-pub-3940256099942544~3347511713';
-  static const iosAppId = 'ca-app-pub-3940256099942544~1458002511';
+  static const androidAppId = 'ca-app-pub-1498129057551982~3865121210';
+  static const iosAppId = 'ca-app-pub-1498129057551982~3865121210';
 
-  static const androidBannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
+  static const androidBannerAdUnitId = 'ca-app-pub-1498129057551982/8216263913';
   static const androidInterstitialAdUnitId =
-      'ca-app-pub-3940256099942544/1033173712';
+      'ca-app-pub-1498129057551982/8216263913';
 
-  static const iosBannerAdUnitId = 'ca-app-pub-3940256099942544/2934735716';
-  static const iosInterstitialAdUnitId = 'ca-app-pub-3940256099942544/4411468910';
+  static const iosBannerAdUnitId = 'ca-app-pub-1498129057551982/8216263913';
+  static const iosInterstitialAdUnitId = 'ca-app-pub-1498129057551982/8216263913';
 }
